a51d36de2fbbec37c6e84db3019947ce5b746608
GioColle4 <spam.giorgiocolletto@gmail.com>
2025-06-24 16:35:34 +0200

    Fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  compress: true,
-  poweredByHeader: false,
-  
-  images: {
-    unoptimized: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
