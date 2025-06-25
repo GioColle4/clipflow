0ef1339d4966220154b6ced60c70b870f2e87adf
GioColle4 <spam.giorgiocolletto@gmail.com>
2025-06-25 12:14:06 +0200

    Configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/clipflow',
+  assetPrefix: '/clipflow/'
+}
 
 export default nextConfig
